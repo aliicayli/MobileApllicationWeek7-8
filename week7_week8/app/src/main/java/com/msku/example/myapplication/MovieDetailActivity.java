d2594b639da31992d3391c5fc1ba07d8548cafb7
aliicayli <aliicayli@gmail.com>
2023-11-29 01:18:11 +0300

    Added mobile application project for week7-8

---
+++ week7_week8/app/src/main/java/com/msku/example/myapplication/MovieDetailActivity.java
@@ -0,0 +1,28 @@
+package com.msku.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.msku.example.myapplication.placeholder.Movie;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                Movie movie = extras.getParcelable("selected_movie");
+                if (movie != null) {
+                    MovieDetailFragment detailFragment = MovieDetailFragment.newInstance(movie);
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.detail_fragment_container, detailFragment)
+                            .commit();
+                }
+            }
+        }
+    }
+}
